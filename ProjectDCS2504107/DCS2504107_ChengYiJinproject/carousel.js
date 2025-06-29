2c1bc0302afb18ba9a1f0150cff39b0678d8adcd
Sam88888-abc <chengyijin0103@gmail.com>
2025-06-29 13:33:49 +0800

    Add files via upload

---
+++ ProjectDCS2504107/DCS2504107_ChengYiJinproject/carousel.js
@@ -0,0 +1,32 @@
+function scrollLeft()  {
+  document.getElementById('carousel').scrollBy({
+    left:-320,
+    behaviour:'smooth'
+  });
+}
+
+function scrollRight() {
+  document.getElementById('carousel').scrollBy({
+    left: 320,
+    behavior: 'smooth'
+  });
+}
+
+// Touch swipe support
+const carousel = document.getElementById('carousel');
+let startX = 0;
+
+carousel.addEventListener('touchstart', (e) => {
+  startX = e.touches[0].clientX;
+});
+
+carousel.addEventListener('touchend', (e) => {
+  const endX = e.changedTouches[0].clientX;
+  const diff = startX - endX;
+
+  if (diff > 50) {
+    scrollRight();
+  } else if (diff < -50) {
+    scrollLeft();
+  }
+});
